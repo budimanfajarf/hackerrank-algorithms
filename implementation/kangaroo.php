c8c5244feecbdeda2eee0165e33124537950befc
budimanfajarf <budimanfajarf@gmail.com>
2020-10-30 10:35:58 +0700

    kangaroo.php :coffee:

---
+++ implementation/kangaroo.php
@@ -0,0 +1,37 @@
+<?php
+
+// Complete the kangaroo function below.
+function kangaroo($x1, $v1, $x2, $v2) {
+    $meet = 'NO';
+
+    for ($i = 0; $i < 10000; $i++) {
+        if ($x1 == $x2)
+            $meet = 'YES';
+        $x1 = $x1 + $v1;
+        $x2 = $x2 + $v2;
+    }
+
+    return $meet;
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$stdin = fopen("php://stdin", "r");
+
+fscanf($stdin, "%[^\n]", $x1V1X2V2_temp);
+$x1V1X2V2 = explode(' ', $x1V1X2V2_temp);
+
+$x1 = intval($x1V1X2V2[0]);
+
+$v1 = intval($x1V1X2V2[1]);
+
+$x2 = intval($x1V1X2V2[2]);
+
+$v2 = intval($x1V1X2V2[3]);
+
+$result = kangaroo($x1, $v1, $x2, $v2);
+
+fwrite($fptr, $result . "\n");
+
+fclose($stdin);
+fclose($fptr);
